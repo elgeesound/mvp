111c59f3d3ba028c94dde4968ff782ec63196c97
elgeesound <elgeesound@gmail.com>
2017-11-10 11:39:17 -0800

    output

--- client/prod/src/components/app.jsx
+++ client/prod/src/components/app.jsx
@@ -16,6 +16,7 @@ class App extends React.Component {
     return (
       <div>
         <Test />
+        <Synth />
       </div>
       )
   }
--- client/prod/src/components/elSynth.jsx
+++ client/prod/src/components/elSynth.jsx
@@ -5,12 +5,15 @@ class Synth extends React.Component {
   constructor(props) {
     super(props);
 
+    this.verb = new Tone.JCReverb(0.9).toMaster();
+    this.crusher = new Tone.BitCrusher(4).connect(this.verb);
+    this.synth = new Tone.AMSynth().connect(this.crusher);
     this.waves = ['sine', 'triangle', 'pulse', 'saw'];
 
     this.state = {
       keys: [{note: 'C4'}, {note: 'D4'}, {note: 'E4'}, {note: 'F4'}],
       toggleOn: false,
-      currentWave: null
+      currentWave: null,
       FX: {
         'BitCrusher': false,
         'Chorus': false,
@@ -29,7 +32,13 @@ class Synth extends React.Component {
 
 
   render(){
+    let keys = this.state.keys;
+    return (
+      <div className="keyboard">
+        {keys.map(key => <button onClick={(e) => this.handleMouseClick(e)}>{key.note}</button>)}
+      </div>
 
+      )
   }
 }
 

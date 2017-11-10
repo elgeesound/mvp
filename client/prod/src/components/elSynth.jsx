5157f00bef1cd880a8d459c0214a0c71c9dc5a61
elgeesound <elgeesound@gmail.com>
2017-11-10 11:19:35 -0800

    press release

--- client/prod/src/components/elSynth.jsx
+++ client/prod/src/components/elSynth.jsx
@@ -2,6 +2,32 @@ import React from 'react';
 import Tone from 'tone';
 
 class Synth extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.waves = ['sine', 'triangle', 'pulse', 'saw'];
+
+    this.state = {
+      keys: [{note: 'C4'}, {note: 'D4'}, {note: 'E4'}, {note: 'F4'}],
+      toggleOn: false,
+      currentWave: null
+      FX: {
+        'BitCrusher': false,
+        'Chorus': false,
+        'Distortion': false,
+        'JCReverb': false,
+        'Phaser': false,
+        'PingPongDelay': false,
+        'StereoWidener': false
+      }
+    }
+  }
+
+  handleMouseClick = (e) => {
+    this.synth.triggerAttackRelease(e.target.textContent, '1n');
+  }
+
+
   render(){
 
   }
